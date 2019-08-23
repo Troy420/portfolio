1781513417928682a48f0e5cd14d37cae119d996
Bernard <bernardy_ho@hotmail.com>
2019-08-23 19:14:43 +0800

    New Look!
    Revamping the whole website

--- assets/js/changetext.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $(".show-more-button").click(function () {
-        $(".show-more-button").fadeOut(function () {
-            $(".show-more-button").text(($(".show-more-button").text() == 'Show More Experiences') ? 'Show Fewer Experiences' : 'Show More Experiences').fadeIn();
-        })
-    })
-});
--- assets/js/slidedown.js
+++
@@ -1,8 +0,0 @@
-// find elements
-var portfolio = $(".section-padding-portfolios")
-var button = $(".show-more-button")
-
-// handle click and add class
-button.on("click", function() {
-  portfolio.toggleClass("down up")
-})
